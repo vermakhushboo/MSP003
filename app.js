7ab0451cd8eb9d91c6e9f12088e700338a82df3d
Mukesh Kumar <mukeshkmr776@gmail.com>
2020-03-30 12:29:42 +0530

    Formatted & improved

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+/* @{Internal Imports} */
+const        path  = require('path');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+/* @{External Imports} */
+const      express = require('express');
+const       logger = require('morgan');
+const cookieParser = require('cookie-parser');
 
-var app = express();
+/* @{ExpressJS} */
+const app = express();
 
+/* @{Express Routes Import} */
+app.use('/'     , require('./routes/index'));
+app.use('/users', require('./routes/users'));
+
+/* @{Middlewares} */
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+/* @{Static Expose} */
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
+/* @{Module Export} */
 module.exports = app;
